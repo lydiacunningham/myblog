fed44a17f717d788fab5fdd77ba7ce46a14e70db
austincunningham <austincunningham@hotmail.com>
2019-04-13 10:29:50 +0100

    adjust the date format

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -32,8 +32,8 @@
             <div class="section" v-for="entry in entries[section]" :key="entry.id">
               <div class="entry">
                 <h3 @click="$router.push({name: entry.id})">
-                  {{entry.title}}
-                  <span class="subtitle">{{entry.date}}</span>
+                  {{entry.title}} 
+                  <span class="subtitle">....({{entry.date}})</span>
                 </h3>
                 <p>{{entry.description}}</p>
               </div>
